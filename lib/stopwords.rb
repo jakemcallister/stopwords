b0067ae7374a6df839174e3c27ca10430110187c
Jake McAllister <jake69mac@gmail.com>
2012-06-20 14:36:40 +0100

    added loads of words

--- lib/stopwords.rb
+++ lib/stopwords.rb
@@ -28,7 +28,9 @@ module Stopwords
     'these','without','besides','however','one','they','would','between','i',
     'only','this','yet','beyond','ie','onto','those','you','both','if','or',
     'though','your','but','in','other','through','yours','by','inc','others',
-    'throughout','yourself','can','indeed','otherwise','thru','yourselves'
+    'throughout','yourself','can','indeed','otherwise','thru','yourselves','bt','jst',
+    'iv','x','xx','xxx','xxxx''xxxxx','ya','yeah','yo','here','oh','ha','haha','haaaa',
+    'ahahaha','go','do','hola','pls','lol','p'
     ]
   TOKEN_REGEXP = /^[a-z]+$|^\w+\-\w+|^[a-z]+[0-9]+[a-z]+$|^[0-9]+[a-z]+|^[a-z]+[0-9]+$/ 
   
